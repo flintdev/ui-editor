793035539de0dc77b527511a986248d99e6a568c
naitianliu <naitianliu@gmail.com>
2020-02-24 14:45:26 -0800

    init project

---
+++ example/ExampleContainer.tsx
@@ -0,0 +1,35 @@
+// example/ExampleContainer.tsx
+
+import * as React from 'react';
+import {withStyles, WithStyles, createStyles} from '@material-ui/core/styles';
+
+const styles = createStyles({
+    root: {
+
+    },
+});
+
+export interface Props extends WithStyles<typeof styles>{
+
+}
+
+class ExampleContainer extends React.Component<Props, object> {
+    state = {
+
+    };
+
+    componentDidMount(): void {
+
+    }
+
+    render() {
+        const {classes} = this.props;
+        return (
+            <div className={classes.root}>
+
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(ExampleContainer);
